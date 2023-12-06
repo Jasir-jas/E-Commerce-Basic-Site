3b2fd241f42c0a7320ed4322113c66bdeb8accf8
Jasir V K <jasivkmohammed@gmail.com>
2023-12-06 19:06:34 +0530

    checkout page all completed..transaction completed and backend datas are correctly set

--- ECOMMERCE/ecommerce_app/urls.py
+++ ECOMMERCE/ecommerce_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("cart/", views.cart,name="cart"),
     path("checkout/", views.checkout,name="checkout"),
     path("update_items/", views.updateItems,name="update_items"),
-    # path("products/<int:pk>/", views.product, name="products"),
+    path("process_order/", views.processOrder,name="process_order"),
+    
     
 ]
--- ECOMMERCE/ecommerce_app/views.py
+++ ECOMMERCE/ecommerce_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import * 
 from django.http import JsonResponse
 import json
+import datetime
 from django.views.decorators.csrf import csrf_exempt
 from json.decoder import JSONDecodeError
 
@@ -73,4 +74,31 @@ def updateItems(request):
     return JsonResponse('item was added',safe=False)
 
 def processOrder(request):
-    return JsonResponse('')
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    print(data)
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer = customer, completed=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        
+        if total == order.get_cart_total:
+            order.completed=True
+        order.save()
+        
+        if order.shipping == True:
+            Shippingaddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+            
+    else:
+        print('user not logged in..')
+            
+    return JsonResponse('payment submitted',safe=False)
